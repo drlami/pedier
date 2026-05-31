1884fcff168696f86c2e3e5f9fbbdd1263302ff8
v0agent <it+v0agent@vercel.com>
2026-05-31 08:53:57 +0000

    feat: update service worker cache revision for index.html

--- artifacts/pmc-pedier-aid/dev-dist/sw.js
+++ artifacts/pmc-pedier-aid/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fl1ab0o5bik"
+    "revision": "0.gvfm1ejggi"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
